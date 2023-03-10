e454bd348f6836f11e677155610e3af2111852e2
권준오 <112926563+ddjunho@users.noreply.github.com>
2023-03-10 15:59:42 +0900

    Update gptbitcoinautotrade.py

--- gptbitcoinautotrade.py
+++ gptbitcoinautotrade.py
@@ -8,19 +8,19 @@ access = "9PofI7vsCCOJEaSaxzZnW79HxcWHnQA2FbrQ7cWQ"
 secret = "diUxCv8gLAl2QQ6Q0RT620as3Vxaon4vYrqyxjMc"
 
 def get_target_price(ticker, k):
-    """변동성 돌파 전략으로 매수 목표가 조회"""
+#변동성 돌파 전략으로 매수 목표가 조회
     df = pyupbit.get_ohlcv(ticker, interval="day", count=2)
     target_price = df.iloc[0]['close'] + (df.iloc[0]['high'] - df.iloc[0]['low']) * k
     return target_price
 
 def get_start_time(ticker):
-    """시작 시간 조회"""
+    #시작 시간 조회
     df = pyupbit.get_ohlcv(ticker, interval="day", count=1)
     start_time = df.index[0]
     return start_time
 
 def get_balance(ticker):
-    """잔고 조회"""
+    #잔고 조회
     balances = upbit.get_balances()
     for b in balances:
         if b['currency'] == ticker:
@@ -31,13 +31,13 @@ def get_balance(ticker):
     return 0
 
 def get_current_price(ticker):
-    """현재가 조회"""
+    #현재가 조회
     return pyupbit.get_orderbook(ticker=ticker)["orderbook_units"][0]["ask_price"]
 
 predicted_sell_price = 0
 
 def predict_price(ticker):
-    """Prophet으로 매도 예측 가격 예측"""
+    #Prophet으로 매도 예측 가격 예측
     global predicted_sell_price
     df = pyupbit.get_ohlcv(ticker, interval="minute60")
     df = df.reset_index()

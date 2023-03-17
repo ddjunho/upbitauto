412279db3d042a371903edd0f63df470ab6e3b45
권준오 <112926563+ddjunho@users.noreply.github.com>
2023-03-17 09:34:17 +0900

    Update gptbitcoinautotrade5.py

--- gptbitcoinautotrade5.py
+++ gptbitcoinautotrade5.py
@@ -50,7 +50,7 @@ def get_current_price(ticker):
     return pyupbit.get_orderbook(ticker=ticker)["orderbook_units"][0]["ask_price"]
 
 def predict_sell_price(ticker):
-    # 3일 동안의 데이터를 가져와서 매도 예측 가격 계산
+    # 7일 동안의 데이터를 가져와서 매도 예측 가격 계산
     df = pyupbit.get_ohlcv(ticker, interval="7", count=7)
     ts = df['close']
     model = sm.tsa.ARIMA(ts, order=(2, 1, 2))

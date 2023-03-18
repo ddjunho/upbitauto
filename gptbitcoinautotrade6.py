c664b9001c800967f4e1891b515c95e387d30384
권준오 <112926563+ddjunho@users.noreply.github.com>
2023-03-18 15:20:30 +0900

    Update gptbitcoinautotrade6.py

--- gptbitcoinautotrade6.py
+++ gptbitcoinautotrade6.py
@@ -32,9 +32,9 @@ def get_balance(ticker):
     # 잔고 조회
     balances = upbit.get_balances()
     for b in balances:
-        if b['currency'] == ticker:
-            if b['balance'] is not None:
-                return float(b['balance'])
+        if b.get('currency') == ticker:
+            if b.get('balance') is not None:
+                return float(b.get('balance'))
             else:
                 return 0
     return 0

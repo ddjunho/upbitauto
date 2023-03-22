3d8c316593ea025fcf4d4dd96185aee2b145de6c
권준오 <112926563+ddjunho@users.noreply.github.com>
2023-03-23 02:20:49 +0900

    Update gptbitcoinautotrade7.py

--- gptbitcoinautotrade7.py
+++ gptbitcoinautotrade7.py
@@ -81,7 +81,7 @@ def run_auto_trade():
                 predicted_sell_price = predict_sell_price(COIN)
                 current_price = get_current_price(COIN)
                 stop_loss = stop_loss(ticker)
-            if target_price >= current_price and target_price < predicted_sell_price and stop_loss < current_price:
+            if target_price >= current_price and target_price < predicted_sell_price and stop_loss > current_price:
                 if get_balance("KRW") < krw * buy_unit:
                     buy_amount = krw * 0.9995
                 upbit.buy_market_order(COIN, buy_amount)

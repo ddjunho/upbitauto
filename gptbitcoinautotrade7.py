286f3fec81ffa7235bcb9baa3bf2bf1a656d1d5d
권준오 <112926563+ddjunho@users.noreply.github.com>
2023-03-23 18:24:35 +0900

    Update gptbitcoinautotrade7.py

--- gptbitcoinautotrade7.py
+++ gptbitcoinautotrade7.py
@@ -9,7 +9,7 @@ from sklearn.preprocessing import MinMaxScaler
 from upbit_keys import access, secret
 
 buy_unit = 0.1   # 분할 매수 금액 단위 설정
-k = 0.35
+k = 0
 COIN = "KRW-BTC" #코인명
 day_s = 0  #15*96은 1일
 
@@ -21,15 +21,14 @@ def vola_break(ticker):
 vola_break_price = vola_break(COIN)
     
 def get_target_price(ticker): #매수최저가예측
-    tf.config.run_functions_eagerly(True)
     # 데이터 불러오기
     df = pyupbit.get_ohlcv(ticker, interval="minute15", count=192)
     # 입력 데이터 전처리
-    X = df[['high', 'low', 'volume']].values  # 입력 데이터는 high, low, volume 3가지 종류
+    X = df[['open', 'high', 'low', 'close', 'volume']].values  # 입력 데이터는 open, high, low, close, volume 5가지 종류
     X_scaler = MinMaxScaler()
     X = X_scaler.fit_transform(X)
     # 출력 데이터 전처리
-    y = df['low'].values  # 출력 데이터는 low 가격
+    y = df['low'].values  # 출력 데이터는 high 가격
     y_scaler = MinMaxScaler()
     y = y_scaler.fit_transform(y.reshape((-1, 1)))
     # 학습 데이터 생성
@@ -42,17 +41,17 @@ def get_target_price(ticker): #매수최저가예측
     y_train = np.array(y_train)
     # Tensorflow 모델 구성
     model = tf.keras.models.Sequential([
-        tf.keras.layers.LSTM(128, input_shape=(192, 3)),
+        tf.keras.layers.LSTM(128, input_shape=(192, 5)),
         tf.keras.layers.Dense(64, activation='relu'),
         tf.keras.layers.Dense(1)
     ])
     # 모델 컴파일
-    model.compile(optimizer='adam', loss='mse', run_eagerly=True)
+    model.compile(optimizer='adam', loss='mse')
     # 학습
     model.fit(X_train, y_train, epochs=100, verbose=0)
     # 새로운 데이터에 대한 예측
-    last_data = df[['high', 'low', 'volume']].iloc[-192:].values  # 가장 최근 192개 데이터
-    last_data = X_scaler.transform(last_data.reshape((1, -1, 3)))  # 입력 데이터 전처리
+    last_data = df[['open', 'high', 'low', 'close', 'volume']].iloc[-192:].values  # 가장 최근 192개 데이터
+    last_data = X_scaler.transform(last_data.reshape((1, -1, 5)))  # 입력 데이터 전처리
     predicted_price = model.predict(last_data)  # 예측 결과
     predicted_price = y_scaler.inverse_transform(predicted_price)
     return predicted_price + vola_break_price
@@ -75,12 +74,11 @@ def get_current_price(ticker):
     except:
         return pyupbit.get_orderbook(ticker=ticker)["orderbook_units"][0]["bid_price"]
     
-def predict_sell_price(ticker): #매도최고가예측
-    tf.config.run_functions_eagerly(True)
+def predict_sell_price(ticker):
     # 데이터 불러오기
     df = pyupbit.get_ohlcv(ticker, interval="minute15", count=192)
     # 입력 데이터 전처리
-    X = df[['high', 'low', 'volume']].values  # 입력 데이터는 high, low, volume 3가지 종류
+    X = df[['open', 'high', 'low', 'close', 'volume']].values  # 입력 데이터는 open, high, low, close, volume 5가지 종류
     X_scaler = MinMaxScaler()
     X = X_scaler.fit_transform(X)
     # 출력 데이터 전처리
@@ -97,17 +95,17 @@ def predict_sell_price(ticker): #매도최고가예측
     y_train = np.array(y_train)
     # Tensorflow 모델 구성
     model = tf.keras.models.Sequential([
-        tf.keras.layers.LSTM(128, input_shape=(192, 3)),
+        tf.keras.layers.LSTM(128, input_shape=(192, 5)),
         tf.keras.layers.Dense(64, activation='relu'),
         tf.keras.layers.Dense(1)
     ])
     # 모델 컴파일
-    model.compile(optimizer='adam', loss='mse', run_eagerly=True)
+    model.compile(optimizer='adam', loss='mse')
     # 학습
     model.fit(X_train, y_train, epochs=100, verbose=0)
     # 새로운 데이터에 대한 예측
-    last_data = df[['high', 'low', 'volume']].iloc[-192:].values  # 가장 최근 192개 데이터
-    last_data = X_scaler.transform(last_data.reshape((1, -1, 3)))  # 입력 데이터 전처리
+    last_data = df[['open', 'high', 'low', 'close', 'volume']].iloc[-192:].values  # 가장 최근 192개 데이터
+    last_data = X_scaler.transform(last_data.reshape((1, -1, 5)))  # 입력 데이터 전처리
     predicted_price = model.predict(last_data)  # 예측 결과
     predicted_price = y_scaler.inverse_transform(predicted_price)
     return predicted_price - vola_break_price
@@ -131,7 +129,7 @@ def run_auto_trade():
                 target_price = get_target_price(COIN)
                 predicted_sell_price = predict_sell_price(COIN)
                 current_price = get_current_price(COIN)
-            if target_price - vola_break_price < current_price and target_price > current_price and target_price < predicted_sell_price:
+            if 'target_price - vola_break_price < current_price' and target_price > current_price and target_price < predicted_sell_price:
                 if get_balance("KRW") < krw * buy_unit:
                     buy_amount = krw * 0.9995
                 upbit.buy_market_order(COIN, buy_amount)

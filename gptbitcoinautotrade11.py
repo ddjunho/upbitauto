e7b4188e70ca290fd56aee4f0aee9d5e3465c522
권준오 <112926563+ddjunho@users.noreply.github.com>
2023-04-07 01:36:18 +0900

    Update gptbitcoinautotrade11.py

--- gptbitcoinautotrade11.py
+++ gptbitcoinautotrade11.py
@@ -46,11 +46,9 @@ def predict_target_price(target_type):
     ticker = input_data['arguments']['ticker']
     target_type = input_data['arguments']['target_type']
     # 데이터 불러오기
-    df1 = pyupbit.get_ohlcv(ticker, interval="minute240", count=183)
-    df2 = pyupbit.get_ohlcv(ticker, interval="minute240", count=183, to=df1.index[0])
-    df3 = pyupbit.get_ohlcv(ticker, interval="minute240", count=183, to=df2.index[0])
-    df4 = pyupbit.get_ohlcv(ticker, interval="minute240", count=183, to=df3.index[0])
-    DF = pd.concat([df4, df3, df2, df1])
+    df1 = pyupbit.get_ohlcv(ticker, interval="minute360", count=183)
+    df2 = pyupbit.get_ohlcv(ticker, interval="minute360", count=183, to=df1.index[0])
+    DF = pd.concat([df2, df1])
     # 입력 데이터 전처리
     X = DF[['open', 'high', 'low', 'close', 'volume']].values
     X_scaler = StandardScaler()
@@ -62,14 +60,14 @@ def predict_target_price(target_type):
     # 학습 데이터 생성
     X_train = []
     y_train = []
-    for i in range(91, len(X)):
-        X_train.append(X[i - 91:i, :])
+    for i in range(365, len(X)):
+        X_train.append(X[i - 365:i, :])
         y_train.append(y[i, 0])
     X_train = np.array(X_train)
     y_train = np.array(y_train)
     # Tensorflow 모델 구성
     model = tf.keras.models.Sequential([
-        tf.keras.layers.LSTM(128, input_shape=(91, 5)),
+        tf.keras.layers.LSTM(128, input_shape=(365, 5)),
         tf.keras.layers.Dense(64, activation='relu'),
         tf.keras.layers.Dense(64, activation='relu'),
         tf.keras.layers.Dense(32, activation='relu'),
@@ -80,18 +78,19 @@ def predict_target_price(target_type):
     # 학습
     model.fit(X_train, y_train, epochs=100, verbose=1)
     # 새로운 데이터에 대한 예측
-    last_data = DF[['open', 'high', 'low', 'close', 'volume']].iloc[-91:].values
+    last_data = DF[['open', 'high', 'low', 'close', 'volume']].iloc[-365:].values
     last_data_mean = last_data.mean(axis=0)
     last_data_std = last_data.std(axis=0)
     last_data = (last_data - last_data_mean) / last_data_std
-    # 예측할 데이터의 shape를 (1, 91, 5)로 변경
+    # 예측할 데이터의 shape를 (1, 365, 5)로 변경
     last_data = np.expand_dims(last_data, axis=0)
     predicted_price = model.predict(last_data)
     predicted_price = y_scaler.inverse_transform(predicted_price)
-    predicted_price = predicted_price.flatten()[0] # 이중 리스트를 일차원으로 변경하고 첫 번째 원소를 선택
+    predicted_price = predicted_price.flatten()[0]  # 이중 리스트를 일차원으로 변경하고 첫 번째 원소를 선택
     return float(predicted_price)
 
 def is_bull_market(ticker):
+    global proba
     df1 = pyupbit.get_ohlcv(ticker, interval="day", count=183)
     df2 = pyupbit.get_ohlcv(ticker, interval="day", count=183, to=df1.index[0])
     DF = pd.concat([df2, df1])
@@ -162,7 +161,7 @@ buy_amount = krw * 0.9995 * buy_unit # 분할 매수 금액 계산
 print("매수가 조회 :",target_price)
 print("매도가 조회 :",sell_price)
 print("현재가 조회 :",current_price)
-print("상승장 예측 :",bull_market)
+print("상승장 예측 :",proba,bull_market)
 print("원화잔고 :",krw)
 print("비트코인잔고 :",btc)
 print("목표가 완화 :",PriceEase*3)

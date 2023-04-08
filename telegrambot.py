aab2ccd7032c35747f05308483a2fa5cb69e1741
권준오 <112926563+ddjunho@users.noreply.github.com>
2023-04-08 13:52:52 +0900

    Update telegrambot.py

--- telegrambot.py
+++ telegrambot.py
@@ -1,8 +1,12 @@
+import asyncio
 from telegram import Bot
+
+async def send_message(bot_token, bot_chat_id, message):
+    bot = Bot(token=bot_token)
+    await bot.send_message(chat_id=bot_chat_id, text=message)
 
 bot_token = "5915962696:AAF14G7Kg-N2tk5i_w4JGYICqamwrUNXP1c" # 봇 토큰
 bot_chat_id = "5820794752" # 채팅 ID
-bot = Bot(token=bot_token)
-
 message = "Hello, World!"
-bot.sendMessage(chat_id=bot_chat_id, text=message)
+
+asyncio.run(send_message(bot_token, bot_chat_id, message))

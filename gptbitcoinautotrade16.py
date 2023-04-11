bbbda10570ce1db7caac9d386b7631b1a95c7287
권준오 <112926563+ddjunho@users.noreply.github.com>
2023-04-11 10:33:07 +0900

    Update gptbitcoinautotrade16.py

--- gptbitcoinautotrade16.py
+++ gptbitcoinautotrade16.py
@@ -158,8 +158,7 @@ last_buy_time = None
 time_since_last_buy = None
 buy_amount = krw * 0.9995 * buy_unit # 분할 매수 금액 계산
 bull_market = is_bull_market(COIN)
-def chat_bot():
-    # proba 값을 Telegram으로 전송
+def send_message_with_code(target_price, sell_price, current_price, proba, bull_market, krw, btc, PriceEase):
     bot_token = "5915962696:AAF14G7Kg-N2tk5i_w4JGYICqamwrUNXP1c" # 봇 토큰
     bot_chat_id = "5820794752" # 채팅 ID
     bot = Bot(token=bot_token)
@@ -168,7 +167,7 @@ def chat_bot():
     if bull_market==True:
         message = "45%이상으로 예측.\n★Autotrade start★"
         bot.send_message(chat_id=bot_chat_id, text=message)
-chat_bot()
+send_message_with_code(target_price, sell_price, current_price, proba, bull_market, krw, btc, PriceEase)
 print("autotrade start")
 # 스케줄러 실행
 while True:
@@ -184,7 +183,7 @@ while True:
             sell_price = predict_target_price(COIN, 'high')
             PriceEase = round((sell_price - target_price) * 0.1, 1)
             bull_market = is_bull_market(COIN)
-            chat_bot()
+            send_message_with_code(target_price, sell_price, current_price, proba, bull_market, krw, btc, PriceEase)
         # 매수 조건
         if current_price <= target_price + PriceEase*2:
             if bull_market==True and krw > 10000 and target_price + PriceEase*2 < sell_price-(PriceEase*3):
